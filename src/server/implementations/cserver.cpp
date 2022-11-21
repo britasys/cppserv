c8d2d45ca1d38bfdae0e0936777da9fb49b42dea
technical <s.hatefinia@tosantechno.com>
2022-11-21 15:47:20 +0330

    Initialization

--- src/server/implementations/cserver.cpp
+++ src/server/implementations/cserver.cpp
@@ -1,39 +1,17 @@
 #include "cserver.hpp"
 
-#include "./beast/beast.hpp"
-
-using namespace __N_BEAST__;
+#include <listener/ilistener.hpp>
 
 namespace __N_SERVER__
 {
     bool CServer::listen() const noexcept
     {
-        // m_listener->run();
-        
-        auto const address = net::ip::make_address("0.0.0.0");
-        auto const port = static_cast<unsigned short>(8080);
-        auto const doc_root = std::make_shared<std::string>("./doc");
-        auto const threads = std::max<int>(1, 5);
-
-        // The io_context is required for all I/O
-        net::io_context ioc{ threads };
+        std::string address = "0.0.0.0";
+        unsigned short port = 8080;
+        unsigned short threads = 5;
 
         // Create and launch a listening port
-        std::make_shared<listener>(
-            ioc,
-            tcp::endpoint{address, port},
-            doc_root)->run();
-
-        // Run the I/O service on the requested number of threads
-        std::vector<std::thread> v;
-        v.reserve(threads - 1);
-        for(auto i = threads - 1; i > 0; --i)
-            v.emplace_back(
-            [&ioc]
-            {
-                ioc.run();
-            });
-        ioc.run();
+        __N_LISTENER__::createListener(address, port, threads)->listen();
         
         return true;
     }
